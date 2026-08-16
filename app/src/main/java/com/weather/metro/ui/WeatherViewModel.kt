73757903f2cf285b6b8c738a9ba32524e4693c29
MaxYu725 <maxyu0725@outlook.com>
2026-08-16 11:36:00 +0800

    feat(notifications): bind journal reconciliation to notification setting

--- app/src/main/java/com/weather/metro/ui/WeatherViewModel.kt
+++ app/src/main/java/com/weather/metro/ui/WeatherViewModel.kt
@@ -16,6 +16,7 @@ import com.weather.metro.data.settings.UiSettings
 import com.weather.metro.domain.LocationInfo
 import com.weather.metro.domain.WeatherLoadState
 import com.weather.metro.notification.NotificationChannels
+import com.weather.metro.notification.NotificationReconcileScheduler
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -91,14 +92,24 @@ class WeatherViewModel(application: Application) : AndroidViewModel(application)
 
     fun setNotificationsEnabled(value: Boolean) {
         settingsRepository.setNotificationsEnabled(value)
+        val application = getApplication<Application>()
         val messaging = FirebaseMessaging.getInstance()
-        if (value) messaging.subscribeToTopic(NotificationChannels.TOPIC_PRODUCTION)
-        else messaging.unsubscribeFromTopic(NotificationChannels.TOPIC_PRODUCTION)
+        if (value) {
+            messaging.subscribeToTopic(NotificationChannels.TOPIC_PRODUCTION)
+            NotificationReconcileScheduler.ensurePeriodic(application)
+            NotificationReconcileScheduler.enqueueNow(application)
+        } else {
+            messaging.unsubscribeFromTopic(NotificationChannels.TOPIC_PRODUCTION)
+            NotificationReconcileScheduler.disable(application)
+        }
     }
 
     fun subscribeIfEnabled() {
         if (settings.value.notificationsEnabled) {
+            val application = getApplication<Application>()
             FirebaseMessaging.getInstance().subscribeToTopic(NotificationChannels.TOPIC_PRODUCTION)
+            NotificationReconcileScheduler.ensurePeriodic(application)
+            NotificationReconcileScheduler.enqueueNow(application)
         }
     }
 
